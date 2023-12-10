0be7c3dcaa0adfccae3e12def6220de472b72b4c
Alibek Allamzharov <alkhero21@MacBook-Pro-Alibek.local>
2023-12-11 00:13:32 +0600

    Create Register

---
+++ chatApp/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  chatApp
+//
+//  Created by Alibek Allamzharov on 10.12.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
